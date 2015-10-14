228c0aa43255605f34923e8f139f0319ad984522
grapek9 <zerex93@gmail.com>
2015-10-14 16:25:37 +0200

    created help window (todo fullfil with information about controlling app and eventually make settings inside help for setting your own buttons)

--- FileBrowserPython/helpWindow.py
+++ FileBrowserPython/helpWindow.py
@@ -1,11 +1,7 @@
 import tkinter as tk
-from helpWindow import *
-def runGui():
-	class MainWindow(tk.Frame):
-		def openHelpWindow():
-			print("wijbdifdskjbf")
-			runHelpWindowGUI();
-			pass
+from tkinter import *
+def runHelpWindowGUI():
+	class helpWindow(tk.Frame):
 		def __init__(self,master=None):
 			tk.Frame.__init__(self,master,width=0,height=0)
 			self.pack()
@@ -15,10 +11,6 @@ def runGui():
 			master.maxsize(width=640,height=480)
 			master.title("FileExplorer by grapek9")
 		def createWidgets(self):
-			def openHelpWindow():
-				print("wijbdifdskjbf")
-				runHelpWindowGUI();
-				pass
 			mainframe = tk.Frame(self,bg="orange",width=640,height=480)
 			mainframe.grid(row=1,column=0)
 			mainframe.pack()
@@ -26,22 +18,16 @@ def runGui():
 			frame1.grid(row=0,column=0,columnspan=60)
 			path = tk.Entry(frame1,width=100)
 			path.grid(row=0,column=0)
-			helpButton = tk.Button(frame1,text="Help")
-			helpButton["command"]= openHelpWindow;
+			helpButton = tk.Button(frame1,text="Help",command="openHelpWindow")
 			helpButton.grid(row=0,column=1)
 			#frame1.pack()
 			frame2 = tk.Frame(mainframe,bg="green",width=100,height=100)
 			frame2.grid(row=1,column=0)
 			#frame2.pack()	
-			def openHelpWindow():
-				print("wijbdifdskjbf")
-				runHelpWindowGUI();
-				pass
-		
+		def openHelpWindow():
+			
+			pass
 	root= tk.Tk()
-	app = MainWindow(master=root)
+	app = helpWindow(master=root)
 	app.mainloop()
 	print("done?")
-	
-
-#reminder = building ui inside frame1
