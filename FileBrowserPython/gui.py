aa2325038bd356d39d0b5e4450ee7c6ba7c05a66
grapek9 <zerex93@gmail.com>
2015-10-15 11:35:32 +0200

    added list with files and folders (need to be fulfilled)

--- FileBrowserPython/gui.py
+++ FileBrowserPython/gui.py
@@ -2,10 +2,6 @@ import tkinter as tk
 from helpWindow import *
 def runGui():
 	class MainWindow(tk.Frame):
-		def openHelpWindow():
-			print("wijbdifdskjbf")
-			runHelpWindowGUI();
-			pass
 		def __init__(self,master=None):
 			tk.Frame.__init__(self,master,width=0,height=0)
 			self.pack()
@@ -16,29 +12,29 @@ def runGui():
 			master.title("FileExplorer by grapek9")
 		def createWidgets(self):
 			def openHelpWindow():
-				print("wijbdifdskjbf")
 				runHelpWindowGUI();
 				pass
 			mainframe = tk.Frame(self,bg="orange",width=640,height=480)
 			mainframe.grid(row=1,column=0)
 			mainframe.pack()
+			#first layer
 			frame1 = tk.Frame(mainframe,width=640)
 			frame1.grid(row=0,column=0,columnspan=60)
 			path = tk.Entry(frame1,width=100)
 			path.grid(row=0,column=0)
-			helpButton = tk.Button(frame1,text="Help")
-			helpButton["command"]= openHelpWindow;
+			helpButton = tk.Button(frame1,text="Help",borderwidth=.003,command=openHelpWindow)
 			helpButton.grid(row=0,column=1)
-			#frame1.pack()
-			frame2 = tk.Frame(mainframe,bg="green",width=100,height=100)
-			frame2.grid(row=1,column=0)
-			#frame2.pack()	
-			def openHelpWindow():
-				print("wijbdifdskjbf")
-				runHelpWindowGUI();
-				pass
-		
+			#second layer
+			frame2 = tk.Frame(mainframe,bg="green",width=640,height=460)
+			frame2.grid(row=1,column=0,sticky="nsew")
+			frame2.grid_rowconfigure(0,weight=1)
+			frame2.grid_columnconfigure(0,weight=1)
+			listbox = tk.Listbox(frame2,height=29)
+			listbox.grid(row=0,column=0,sticky=(N,W,E,S))
+			listbox.grid_columnconfigure(0, weight=1)
+			listbox.grid_rowconfigure(0, weight=1)
 	root= tk.Tk()
+	root["bg"] = "grey"
 	app = MainWindow(master=root)
 	app.mainloop()
 	print("done?")
